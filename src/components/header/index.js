020d046dd38ec385399ad5816c2ed78b44c8650a
Simon Knott <simoknott@gmail.com>
2017-09-23 11:49:22 +0200

    Initial Commit

---
+++ src/components/header/index.js
@@ -0,0 +1,28 @@
+import { h } from 'preact';
+import { Link } from 'preact-router/match';
+
+import Toolbar from 'preact-material-components/Toolbar';
+import 'preact-material-components/Toolbar/style.css';
+
+const Header = (props, state) => (
+	<Toolbar className="toolbar">
+		<Toolbar.Row>
+			<Toolbar.Section align-start>
+				<Toolbar.Title>
+					<Link href="/" style={{ color: 'white' }}>
+						vplan
+					</Link>
+				</Toolbar.Title>
+			</Toolbar.Section>
+			<Toolbar.Section align-end>
+				<Toolbar.Icon>
+					<Link href="/settings/" style={{ color: 'white' }}>
+						Settings
+					</Link>
+				</Toolbar.Icon>
+			</Toolbar.Section>
+		</Toolbar.Row>
+	</Toolbar>
+);
+
+export default Header;
---
+++ src/routes/settings/index.js
@@ -0,0 +1,54 @@
+import { h, Component } from 'preact';
+import style from './style';
+
+class Settings extends Component {
+	stufen = ['5A', '5B', '5C', '5D', '6A', '6B', '6C', '6D', '7A', '7B', '7C', '7D', '8A', '8B', '8C', '8D', '9A', '9B', '9C', '9D', 'EF', 'Q1', 'Q2']
+		.map(item => <option>{item}</option>)
+
+	handleChange = event => {
+		this.setState({
+			klasse: event.target.value
+		});
+
+		this.updateLocalStorage();
+	}
+
+	updateLocalStorage = () => {
+		localStorage.setItem('klasse', JSON.stringify(this.state.klasse));
+	}
+	
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+
+	componentWillMount() {
+		const klasse = JSON.parse(localStorage.getItem('klasse'));
+		this.state = {
+			klasse
+		};
+	}
+
+	componentDidUpdate(nextProps,nextState) {
+		this.updateLocalStorage();
+	}
+
+	render() {
+		
+		return (
+			<div class={style.home}>
+				<form>
+					<label>
+						Klasse:
+						<select onChange={this.handleChange}>
+							{this.stufen}
+						</select>
+					</label>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default Settings;
