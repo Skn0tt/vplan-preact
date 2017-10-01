d9c6df0f5ddd54ccfc1eaadba00dee658f49c988
Simon Knott <simoknott@gmail.com>
2017-10-01 21:30:31 +0200

    Fix offline try to reload

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -24,7 +24,7 @@ class Home extends Component {
 				)) : [];
 
 	refreshData = () => {
-		if (isBrowser) {
+		if (isBrowser && navigator.onLine) {
 			this.setState({ refreshing: true });
 
 			const proxy = 'https://cors-anywhere.herokuapp.com/';
