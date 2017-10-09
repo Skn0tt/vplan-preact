3bbbb8c95590b5de7e2c54a3b72364e2b02a232d
Simon Knott <simoknott@gmail.com>
2017-10-09 17:26:59 +0200

    Move Marked cache to settings

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -19,8 +19,7 @@ class Home extends Component {
 				.sort((a, b) => a.date.getTime() - b.date.getTime())
 				.map(item => (
 					<Item
-						onMark={this.props.onMark}
-						marked={this.props.marked.includes(item.fach)}
+						marked={this.state.marked.includes(item.fach)}
 						{...item}
 					/>
 				))
@@ -61,11 +60,13 @@ class Home extends Component {
 		if (isBrowser) {
 			let items = JSON.parse(localStorage.getItem('items')) || [];
 			let klasse = JSON.parse(localStorage.getItem('klasse')) || 'Q1';
+			let marked = JSON.parse(localStorage.getItem('marked')) || [];
 	
 			this.state = {
 				refreshing: false,
 				items,
-				klasse
+				klasse,
+				marked
 			};
 		}
 	}
